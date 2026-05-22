784130751b5798eebd914caf4fd56e7a54c4de1c
radiolab <48355256+radiolab81@users.noreply.github.com>
2026-05-22 04:50:11 +0000

    Delete ffmpeg_streams.sh

--- ffmpeg_streams.sh
+++
@@ -1,3 +0,0 @@
-ffmpeg -re -i /media/user/HDD2/Radiochannels/Linda_Abba_Ch/01-Rebelle.m4a \
-    -af "lowpass=f=4500,pan=10c|c0=c0|c1=c1|c2=c0|c3=c1|c4=c0|c5=c1|c6=c0|c7=c1|c8=c0|c9=c1,aresample=12500" \
-    -f u8 -acodec pcm_u8 udp://127.0.0.1:4444
